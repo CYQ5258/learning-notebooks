894b3270840288a698a8e780808a1378c3261c8a
Chern_勇庆 <18236517211@139.com>
2020-01-20 13:02:32 +0800

    spyter notebook notes
    computer science learning notes

---
+++ computer-science/Object-oriented Programming/002电子秤代码实现.cpp
@@ -0,0 +1,16 @@
+//
+//  C++test.cpp
+//  ChernApp
+//
+//  Created by mac on 2020/1/16.
+//  Copyright © 2020 yongqing.wenji@gmail.com. All rights reserved.
+//
+
+#include "C++test.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
